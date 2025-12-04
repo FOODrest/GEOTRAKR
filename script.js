0afa4812f154e9e8e9f26ec87bce1583afc31494
Saad <saadrajab110@gmail.com>
2025-12-04 17:26:01 +0100

    code initial

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+  Fichier de base GeoTrakr
+  TODO : 
+  - Créer les classes Session, RunSession, BikeSession
+  - Créer la classe ActivityManager
+  - Gérer la géolocalisation et l'affichage de la carte Leaflet
+  - Gérer le formulaire, les événements, le localStorage, etc.
+*/
+
+// Exemple : point d'entrée de l'application
+// Vous déciderez si vous utilisez une classe principale ou une approche différente.
+
+console.log("GeoTrakr - point de départ du projet");

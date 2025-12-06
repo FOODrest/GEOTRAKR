b4c3b0ce04316db10ee61c805e777ab810c812be
YousriEL <youriabaoui@gmail.com>
2025-12-06 12:41:41 +0100

    Testing the geolocalisation API

--- script.js
+++ script.js
@@ -13,3 +13,50 @@
 // Vous déciderez si vous utilisez une classe principale ou une approche différente.
 
 console.log("GeoTrakr - point de départ du projet");
+
+/*
+if ("geolocation" in navigator){
+  navigator.geolocation.getCurrentPosition(
+    function (position){
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Position trouvée : ${latitude}, ${longitude}`);
+      const map = L.map('map').setView([latitude,longitude],15);
+      L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+      }
+      ).addTo(map);
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("📍 You are here !")
+        .openPopup();
+
+    }
+  )
+
+}
+*/
+
+class Session{
+  constructor({coords, distance, duration, type}){
+    this.id = (Date.now()+'').slice(-10);
+    this.date = new Date();
+    this.coords = coords;//[lat,lng]
+    this.distance =+distance;
+    this.duration =+duration;
+    this.type = type;
+  }
+  getSummary(){
+    return {
+      id: this.id,
+      date: this.date.toISOString(),
+      type: this.type,
+      distance: this.distance,
+      duration: this.duration
+    };
+
+  }
+
+}
